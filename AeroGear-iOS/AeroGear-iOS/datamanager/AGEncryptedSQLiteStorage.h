0ab9676c6132dc7bebd66251b384c7bedb3041a1
corinnekrych <corinnekrych@gmail.com>
2013-12-17 11:58:25 +0100

    add documentatation

--- AeroGear-iOS/AeroGear-iOS/datamanager/AGEncryptedSQLiteStorage.h
+++ AeroGear-iOS/AeroGear-iOS/datamanager/AGEncryptedSQLiteStorage.h
@@ -17,6 +17,38 @@
 
 #import <Foundation/Foundation.h>
 #import "AGSQLiteStorage.h"
+/**
+ An AGStore implementation that uses a SQLite for storage and encryption. This storage is a variant of AGSQLiteStorage
+  using AGPassphraseKeyServices. You can use your encrypted store transparently, the same way you work with AGSQLiteStorage.
 
+ *NOTE:*
+ You must adhere to the rules governing the serialization of data types for each respective field type.
+
+ *IMPORTANT:* Users are not required to instantiate this class directly, instead an instance of this class is returned automatically when an DataStore with the _type_ config option is set to _"SQLITE"_. See AGDataManager and AGStore class documentation for more information.
+
+ ## Create a DataManager with an encrypted SQLite store backend
+
+ Below is a small example on how to use SQLite:
+
+    // initalize SQLite store (if the file does not exist it will be created)
+    AGDataManager* manager = [AGDataManager manager];
+    id<AGStore> store = [manager store:^(id<AGStoreConfig> config) {
+      [config setName:@"secrets"]; // will be used as the filename for the sqlite database.
+      [config setType:@"ENCRYPTED_SQLITE"];  // specify you want to use Encrypted SQLite store.
+    }];
+
+    // the object to save (e.g. a dictionary)
+    NSDictionary *otp = [NSDictionary dictionaryWithObjectsAndKeys:@"19a01df0281afcdbe", @"otp", @"1", @"id", nil];
+
+    // save it
+    NSError *error;
+
+    if (![store save:otp error:&error])
+    NSLog(@"Save: An error occured during save! \n%@", error);
+
+
+ The ```read```, ```reset``` or ```remove``` methods found in AGStore behave the same, as on the default ("in memory") store.
+
+ */
 @interface AGEncryptedSQLiteStorage : AGSQLiteStorage
 @end
