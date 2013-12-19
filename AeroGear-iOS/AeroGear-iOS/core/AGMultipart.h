5cce5953c02bbe5ace5dcd9d14e27d5de6c0ad9b
Christos Vasilakis <cvasilak@gmail.com>
2013-12-19 16:07:01 +0200

    refactor to a multipart interface that can support more than one type

---
+++ AeroGear-iOS/AeroGear-iOS/core/AGMultipart.h
@@ -0,0 +1,105 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright Red Hat, Inc., and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+/**
+ * A Protocol that multipart object must implement.
+ */
+@protocol AGMultipart <NSObject>
+
+/**
+ * The name to be associated with this multipart object.
+ */
+@property (nonatomic, readonly, copy) NSString *name;
+
+/**
+ * The filename to be associated with this multipart object.
+ */
+@property (nonatomic, readonly, copy) NSString *fileName;
+
+/**
+ * The mimeType to be associated with this multipart object.
+ */
+@property (nonatomic, readonly, copy) NSString *mimeType;
+
+@end
+
+/**
+ * A multipart object that it's contents are initialized
+ * from a file on the file system.
+ */
+@interface AGFilePart : NSObject <AGMultipart>
+
+@property (nonatomic, readonly) NSURL *fileURL;
+
+/**
+ * Initialize a multipart object that points to a file on the file system.
+ *
+ * @param fileURL The URL of the file whose contents will be added on the request.
+ * @param name    The name to be associated with the specified data.
+ */
+- (id)initWithFileURL:(NSURL *)fileURL
+                 name:(NSString *)name;
+@end
+
+/**
+ * A multipart object that it's contents are initialized
+ * from an NSData object
+ */
+@interface AGFileDataPart :  NSObject <AGMultipart>
+
+@property (nonatomic, readonly) NSData *data;
+
+/**
+ * Initialize a multipart object from an data object.
+ *
+ * @param data     The data whose contents will be added on the request.
+ * @param name     The name to be associated with the specified data.
+ * @param fileName The filename to be associated with the specified data.
+ * @param mimeType The MIME type to be associated with the specified data.
+ */
+- (id)initWithFileData:(NSData *)data
+                  name:(NSString *)name
+              fileName:(NSString *)fileName
+              mimeType:(NSString *)mimeType;
+@end
+
+/**
+ * A multipart object that it's contents are initialized
+ * from a I/O stream.
+ */
+@interface AGStreamPart :  NSObject <AGMultipart>
+
+@property (nonatomic, readonly) NSInputStream *inputStream;
+@property (nonatomic, assign) unsigned long long length;
+
+/**
+ * Initialize a multipart object from an input stream.
+ *
+ * @param inputStream The inputstream whose contents will be added on the request.
+ * @param name        The name to be associated with the specified data.
+ * @param fileName    The filename to be associated with the specified data.
+ * @param length      The total length of bytes of this input stream
+ * @param mimeType    The MIME type to be associated with the specified data.
+ */
+- (id)initWithInputStream:(NSInputStream *)inputStream
+                     name:(NSString *)name
+                 fileName:(NSString *)fileName
+                   length:(unsigned long long)length
+                 mimeType:(NSString *)mimeType;
+@end
---
+++ AeroGear-iOS/AeroGear-iOS/core/AGMultipart.m
@@ -0,0 +1,114 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright Red Hat, Inc., and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "AGMultipart.h"
+
+@implementation AGFilePart
+
+@synthesize name =_name;
+@synthesize fileName = _fileName;
+@synthesize mimeType = _mimeType;
+
+- (id)initWithFileURL:(NSURL *)fileURL
+                 name:(NSString *)name {
+    
+    self = [super init];
+    
+    if (self) {
+        _name = name;
+
+        // extract filename from URL
+        NSString *fileName = [fileURL lastPathComponent];
+        
+        _fileName = fileName;
+        _mimeType = [AGFilePart getMimeTypeForName:fileName];
+        _fileURL = fileURL;
+    }
+    
+    return self;
+}
+            
+// utility method to extract the mime type from a filename extension
++ (NSString *)getMimeTypeForName:(NSString *)filename {
+    #ifdef __UTTYPE__
+        NSString *extension = [filename stringByDeletingPathExtension];
+    
+        NSString *UTI = (__bridge_transfer NSString *) UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)extension, NULL);
+        NSString *contentType = (__bridge_transfer NSString *)UTTypeCopyPreferredTagWithClass((__bridge CFStringRef)UTI, kUTTagClassMIMEType);
+        if (!contentType) {
+            return @"application/octet-stream";
+        } else {
+            return contentType;
+        }
+    #else
+        return @"application/octet-stream";
+    #endif
+}
+
+@end
+
+@implementation AGFileDataPart
+
+@synthesize name =_name;
+@synthesize fileName = _fileName;
+@synthesize mimeType = _mimeType;
+
+- (id)initWithFileData:(NSData *)data
+                  name:(NSString *)name
+              fileName:(NSString *)fileName
+              mimeType:(NSString *)mimeType {
+    
+    self = [super init];
+    
+    if (self) {
+        _data = data;
+        _name = name;
+        _fileName = fileName;
+        _mimeType = mimeType;
+    }
+    
+    return self;
+}
+
+@end
+
+@implementation AGStreamPart
+
+@synthesize name =_name;
+@synthesize fileName = _fileName;
+@synthesize mimeType = _mimeType;
+
+- (id)initWithInputStream:(NSInputStream *)inputStream
+                     name:(NSString *)name
+                 fileName:(NSString *)fileName
+                   length:(unsigned long long)length
+                 mimeType:(NSString *)mimeType {
+    
+    self = [super init];
+    
+    if (self) {
+        _inputStream = inputStream;
+        _name = name;
+        _fileName = fileName;
+        _length = length;
+        _mimeType = mimeType;
+    }
+    
+    return self;    
+}
+
+@end
