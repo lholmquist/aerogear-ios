f9e227037d626d533ea9c6d384c12a04a0e0bbe5
corinnekrych <corinnekrych@gmail.com>
2013-12-20 08:38:51 +0100

    add documentation in AGAuthzModule header file

--- AeroGear-iOS/AeroGear-iOS/security/Authorizer/AGAuthzModule.h
+++ AeroGear-iOS/AeroGear-iOS/security/Authorizer/AGAuthzModule.h
@@ -18,92 +18,38 @@
 #import <Foundation/Foundation.h>
 
 /**
-AGAuthenticationModule represents an authentication module and provides the authentication and enrollment API. The
-default implementation uses REST as the auth transport. Similar to the [Pipe](AGPipe), technical details of the
+AGAuthzModule represents an OAuth2 module and provides an interface for managing OAuth2 dance. The
+default implementation uses REST as the authz transport. Similar to the [Pipe](AGPipe), technical details of the
 underlying system are not exposed.
 
-## Register a user
-
-The ```enroll``` function of the _AGAuthenticationModule_ protocol is used to register new users with the backend:
-
-    // assemble the dictionary that has all the data for THIS particular user:
-    NSMutableDictionary* userData = [NSMutableDictionary dictionary];
-    [userData setValue:@"john" forKey:@"username"];
-    [userData setValue:@"123" forKey:@"password"];
-    [userData setValue:@"me@you.com" forKey:@"email"];
-    [userData setValue:@"21sda812sad24" forKey:@"betaAccountToken"];
-
-    // register a new user
-    [myMod enroll:userData success:^(id data) {
-        // after a successful _registration_, we can work
-        // with the returned data...
-        NSLog(@"We got: %@", data);
-    } failure:^(NSError *error) {
-        // when an error occurs... at least log it to the console..
-        NSLog(@"SAVE: An error occured! \n%@", error);
-    }];
-
-The ```enroll``` function submits a generic map object which contains all the information about the new user, that the
-server endpoint requires. The default (REST) auth module issues for the above, a request against
-_https://todo-aerogear.rhcloud.com/todo-server/auth/enroll_. Besides the NSDictionary, the function accepts two simple
-blocks that are invoked on success or in case of an failure.
-
-## Login 
+## Login
 
 Once you have a _valid_ user you can use that information to issue a login against the server, to start accessing
 protected endpoints:
 
-    // issuing a login
-    [myMod login:@{@"username": @"john", @"password": @"123"} success:^(id object) {
-        // after a successful _login_, we can work
-        // with the returned data...
+    AGAuthorizer* authorizer = [AGAuthorizer authorizer];
+
+    _restAuthzModule = [authorizer authz:^(id<AGAuthzConfig> config) {
+        config.name = @"restAuthMod";
+        config.baseURL = [[NSURL alloc] initWithString:@"https://accounts.google.com"];
+        config.authzEndpoint = @"/o/oauth2/auth";
+        config.accessTokenEndpoint = @"/o/oauth2/token";
+        config.clientId = @"XXXXXXX";
+        config.clientSecret = @"XXXXXXX";
+        config.redirectURL = @"org.aerogear.GoogleDrive:/oauth2Callback";
+        config.scopes = @[@"https://www.googleapis.com/auth/drive"];
+    }];
+
+    [_restAuthzModule requestAccess:nil success:^(id object) {
+       // object contain the access token needed to query exposed web service
+       // You can use ipe as usual
     } failure:^(NSError *error) {
-        // when an error occurs... at least log it to the console..
-        NSLog(@"SAVE: An error occured! \n%@", error);
+        // an error occurred
     }];
+}
 
-The ```login``` function submits a generic map object which contains the credentials that the server login service
-requires and accepts two simple blocks that are invoked on success or in case of a failure to login.
- 
-The default (REST) auth module issues for the above, a request against _https://todo-aerogear.rhcloud.com/todo-server/auth/login
-
-## Pass the auth module to a pipe
-
-After running a successful login, you can start using the _AGAuthenticationModule_ object on a _AGPipe_ object to access
-protected endpoints:
-
-    ...
-    id<AGPipe> tasks = [pipeline pipe:^(id<AGPipeConfig> config) {
-        [config setName:@"tasks"];
-        [config setBaseURL:serverURL];
-        [config setAuthModule:myMod];
-    }];
-
-    [tasks read:^(id responseObject) {
-        // LOG the JSON response, returned from the server:
-        NSLog(@"READ RESPONSE\n%@", [responseObject description]);
-    } failure:^(NSError *error) {
-        // when an error occurs... at least log it to the console..
-        NSLog(@"Read: An error occured! \n%@", error);
-    }];
-
-When creating a [Pipe](AGPipe) you need to use the _authModule_ argument in order to pass in an _AGAuthenticationModule_
-object.
-
-## Logout
-
-The logout from the server can be archived by using the ```logout``` function:
-
-    // logout:
-    [myMod logout:^{
-        // after a successful _logout_, when can notify the application
-    } failure:^(NSError *error) {
-        // when an error occurs... at least log it to the console..
-        NSLog(@"SAVE: An error occured! \n%@", error);
-    }];
-
-The default (REST) auth module issues for the above a request against _https://todo-aerogear.rhcloud.com/todo-server/auth/logout_.
-The function accepts two simple blocks that are invoked on success or in case of an failure.
+The ```requestAccess:success:failure:``` function deal with the OAuth dance. To manage callback to application,
+the AeroGear uses URL scheme. Schema should be carefully defined in you plist application.
 
 ## Time out and Cancel pending operations
 
@@ -112,51 +58,16 @@ _AGAuthenticationModule_ is supported too.
  */
 @protocol AGAuthzModule <NSObject>
 
-/**
- * Returns the type of the underlying 'auth module implementation'
- */
+
 @property (nonatomic, readonly) NSString* type;
-
-/**
- * Returns the baseURL string of the underlying 'auth module implementation'
- */
 @property (nonatomic, readonly) NSString* baseURL;
-
-/**
- * Returns the 'login endpoint' of the underlying 'auth module implementation'
- */
 @property (nonatomic, readonly) NSString* authzEndpoint;
 @property (nonatomic, readonly) NSString* accessTokenEndpoint;
-
-/**
- * Returns the 'logout endpoint' of the underlying 'auth module implementation'
- */
 @property (nonatomic, readonly) NSString* redirectURL;
-
-/**
- * Returns the 'enroll endpoint' of the underlying 'auth module implementation'
- */
 @property (nonatomic, readonly) NSArray* scopes;
-
 @property (nonatomic, readonly) NSString* clientId;
-
 @property (nonatomic, readonly) NSString* clientSecret;
 
-/**
- * Performs the login for the given user. Since the data will be sent in plaintext, it is IMPORTANT,
- * to run the sign-in via TLS/HTTPS.
- *
- * @param loginData A map (NSDictionary) containing the credentials required by the
- * 'log in' service.
- *
- * @param success A block object to be executed when the operation finishes successfully.
- * This block has no return value and takes one argument: A collection (NSDictionary), containing the response
- * from the 'login' service.
- *
- * @param failure A block object to be executed when the operation finishes unsuccessfully.
- * This block has no return value and takes one argument: The `NSError` object describing
- * the error that occurred.
- */
 -(void) requestAccess:(NSDictionary*) extraParameters
      success:(void (^)(id object))success
      failure:(void (^)(NSError *error))failure;
